b97da44aaff7d2cf1092427ad229c8216f9a250a
JayWebDevCom <jaiye.s@gmail.com>
2017-06-19 15:03:05 +0100

    image text test

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ var app = express();
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
-var jsFile = require('./js/passphrase')
-
 app.listen(8080, function(){
   console.log('port is 8080!')
 })
 
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname));
+
+var ourImage = './images/picOne.jpg'
 
 app.get('/', function(req, res){
-  res.render('index')
+  res.render('index', {
+    ourImage: ourImage
+  })
 })
 
 app.post('/anthony', function(req, res){
   console.log(req.body.text)
+  if (req.body.text == require('./js/passphrase')) {
+    return res.redirect('/confirmed')
+  }
+})
+
+app.get('/confirmed', function(req, res){
+  res.render('confirmed')
 })
